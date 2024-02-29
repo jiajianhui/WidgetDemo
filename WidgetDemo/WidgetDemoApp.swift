ceb4f3c06d7c3c17133535808af78d37050b8172
jiajianhui <jia15176168273@icloud.com>
2024-02-29 11:20:22 +0800

    Initial Commit

---
+++ WidgetDemo/WidgetDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetDemoApp.swift
+//  WidgetDemo
+//
+//  Created by 贾建辉 on 2024/2/29.
+//
+
+import SwiftUI
+
+@main
+struct WidgetDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
